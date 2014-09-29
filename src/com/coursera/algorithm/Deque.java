8d0de6c999ed94a4acb8adb081e914e0a22b70b2
koustuvsinha <koustuvs@nrifintech.com>
2014-09-29 15:13:48 +0530

    Updated

--- src/com/coursera/algorithm/Deque.java
+++ src/com/coursera/algorithm/Deque.java
@@ -10,6 +10,7 @@ public class Deque<Item> implements Iterable<Item> {
 	private int arrayLength = 0;
 	private int arrayFirst = 0;
 	private int arrayLast = 0;
+	private int position = 0;
 	
 	/**
 	 * Constructor to construct an empty deque
@@ -49,11 +50,25 @@ public class Deque<Item> implements Iterable<Item> {
 	 */
 	public void addFirst(Item item) {
 		
-		if(deque.length == arrayLength) {
+		if(item == null)
+			throw new NullPointerException();
+		
+		if(size() == arrayLength) {
 			increaseArrayLength();
 		}
 		
-		deque[--arrayFirst] = item;
+		if(arrayFirst == 0) {
+			reArrangeArray();
+			deque[0] = item;
+			arrayFirst = 0;
+		}
+		else {
+			deque[--arrayFirst] = item;
+		}
+		
+		
+		
+		
 	}
 	
 	/**
@@ -61,7 +76,10 @@ public class Deque<Item> implements Iterable<Item> {
 	 */
 	public void addLast(Item item) {
 		
-		if(deque.length == arrayLength) {
+		if(item == null)
+			throw new NullPointerException();
+		
+		if(size() == arrayLength) {
 			increaseArrayLength();
 		}
 		
@@ -73,14 +91,37 @@ public class Deque<Item> implements Iterable<Item> {
 	 * @return delete and return the item at the front
 	 */
 	public Item removeFirst() {
-		return null;
+		
+		Item first = null;
+		
+		if(arrayFirst == 0) {
+			first = deque[0];
+			shiftArrayLeft();
+		} else {
+			first = deque[arrayFirst--];
+		}
+		
+		
+		
+		if(size()<=arrayLength/4) {
+			decreaseArrayLength();
+		}
+		
+		return first;
 	}
 	
 	/**
 	 * @return delete and return the item at the end
 	 */
 	public Item removeLast() {
-		return null;
+		
+		Item last = deque[--arrayLast];
+		
+		if(size()<=arrayLength/4) {
+			decreaseArrayLength();
+		}
+		
+		return last;
 	}
 	
 	/* return an iterator over items in order from front to end
@@ -90,7 +131,8 @@ public class Deque<Item> implements Iterable<Item> {
 	@Override
 	public Iterator<Item> iterator() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return new DequeIterator<Item>();
 	}
 	
 	/**
@@ -112,6 +154,44 @@ public class Deque<Item> implements Iterable<Item> {
 		
 	}
 	
+	@SuppressWarnings("unchecked")
+	private void reArrangeArray() {
+		
+		Item[] newArray = null;
+		
+		if(size()==arrayLength) {
+			newArray = (Item[])new Object[arrayLength*ARRAY_INCREASE_FACTOR];
+		} else {
+			newArray = (Item[])new Object[arrayLength];
+		}
+		
+		
+		for(int i=0;i<arrayLast;i++) {
+			newArray[i+1] = deque[i];
+		}
+		
+		arrayLast++;
+		
+		deque = newArray;
+		newArray = null;
+		
+	}
+	
+	private void shiftArrayLeft() {
+		
+		@SuppressWarnings("unchecked")
+		Item[] newArray = (Item[])new Object[arrayLength];
+		
+		for(int i=0;i<arrayLast-1;i++) {
+			newArray[i] = deque[i+1];
+		}
+		
+		arrayLast--;
+		deque = newArray;
+		newArray = null;
+		
+	}
+	
 	/**
 	 * Reduce the array by half
 	 */
@@ -130,11 +210,79 @@ public class Deque<Item> implements Iterable<Item> {
 		arrayLength = arrayLength/ARRAY_INCREASE_FACTOR;
 	}
 	
+	
+	@SuppressWarnings("hiding")
+	private class DequeIterator<Item> implements Iterator<Item> {
+
+		@Override
+		public boolean hasNext() {
+			
+			if (position < arrayLast) {
+				return true;
+			} else {
+				return false;
+			}
+			
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Item next() {
+			
+			if(position < arrayLast) {
+			
+				return (Item) deque[position++];
+			
+			} else {
+				
+				throw new java.util.NoSuchElementException(); 
+				
+			}
+			
+		}
+
+		@Override
+		public void remove() {
+			
+			throw new UnsupportedOperationException();
+			
+		}
+		
+	}
+	
 	/**
 	 * @param args unit testing
 	 */
 	public static void main(String[] args) {
 		
+		Deque<Integer> dequeArray = new Deque<Integer>();
+		
+		dequeArray.addLast(new Integer(6));
+		dequeArray.addLast(new Integer(10));
+		dequeArray.addFirst(new Integer(3));
+		dequeArray.addFirst(new Integer(2));
+		dequeArray.addFirst(new Integer(1));
+		dequeArray.addFirst(new Integer(0));
+		dequeArray.addLast(new Integer(11));
+		dequeArray.addLast(new Integer(12));
+		dequeArray.addLast(new Integer(13));
+		dequeArray.addLast(new Integer(14));
+		dequeArray.addLast(new Integer(15));
+		
+		System.out.println("Removing items...");
+		System.out.println(dequeArray.removeLast());
+		System.out.println(dequeArray.removeLast());
+		System.out.println(dequeArray.removeLast());
+		System.out.println(dequeArray.removeFirst());
+		System.out.println(dequeArray.removeLast());
+		//System.out.println(dequeArray.removeLast());
+		
+		Iterator<Integer> iterator = dequeArray.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next().toString());
+		}
+		
 	}
+	
 	
 }
